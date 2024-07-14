669cea718bade2aa62b1e7f6d1609b2d6ec43ffd
Jakub Horáček <horacek2001@gmail.com>
2024-07-14 15:43:11 +0200

    Add a footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,15 @@ export default function Home() {
   return (
     <main>
       <List></List>
+      <footer>
+        <p className="footer__paragraph">
+          <a href="https://linktr.ee/jamess_hillman" target="_blank">
+            Jakub Horáček
+          </a>
+          <span>|</span>
+          &copy;2024
+        </p>
+      </footer>
     </main>
   );
 }
